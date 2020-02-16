757b056d9459487349e50c77bb854a606ff2488e
phaserush <leozhang888@yahoo.ca>
2020-02-15 21:26:41 -0800

    Tried typescript, cant get it working

---
+++ node_modules/@types/p5/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/p5`
+
+# Summary
+This package contains type definitions for p5 (https://github.com/processing/p5.js).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/p5
+
+Additional Details
+ * Last updated: Tue, 06 Aug 2019 23:10:00 GMT
+ * Dependencies: none
+ * Global values: none
+
+# Credits
+These definitions were written by p5-types <https://github.com/p5-types>, and Pierre Krafft <https://github.com/Zalastax>.
---
+++ src/RotBox.ts
@@ -0,0 +1,38 @@
+import * as p5 from 'p5'
+
+const rotBox = function (p: p5) {
+    const strokeWeight = 150;
+    const transX = 3000;
+    const transY = 300;
+    const transZBase = 1000;
+    const squareSideBase = 1000;
+
+
+    let wheelModifier = 1.0;
+    let modifier = 1.0;
+    let C;
+
+    p.setup = () => {
+        p.createCanvas(p.displayWidth, p.displayHeight, p.WEBGL);
+        p.strokeWeight(strokeWeight * 2);
+        C = 0;
+    };
+    p.draw = () => {
+        const R = p.PI / 12 * modifier;
+        p.clear();
+        C++;
+        p.rotateZ(R);
+        p.rotateX(-R);
+        p.rotateY(-R);
+        p.translate(transX, transY, transZBase + (C % 8) * 100 / 8);
+        for (let i = 0; i < 64; i++) {
+            p.stroke(i * 8 * wheelModifier - C % 8);
+            for (let j = 0; j < 15; j++) {
+                p.square(-j * squareSideBase - (C * 3) % squareSideBase, 0, 750);
+            }
+            p.translate(0, 0, -100);
+        }
+    };
+};
+
+new p5(rotBox);
