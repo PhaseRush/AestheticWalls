6b9015a0b5cee067b6cd5cd71fb2b789cac8e23c
phaserush <leozhang888@yahoo.ca>
2020-03-13 21:31:37 -0700

    Vertex refactor, move helpers to bottom

--- src/vertexplane/sketch.ts
+++ src/vertexplane/sketch.ts
@@ -40,6 +40,51 @@ new P5((p: P5) => {
         updateKdTree();
     }
 
+    function render(v1: Vertex, v2: Vertex, v3: Vertex) {
+        if (fillShape) {
+            _.stroke(255, 10);
+            _.fill(determineColour(v1, v2, v3));
+        } else {
+            _.noFill();
+            _.strokeWeight(1);
+            _.stroke(determineColour(v1, v2, v3));
+        }
+        // shape
+        _.beginShape(0x0004); // 4 ==="TRIANGLES" hardcoded because global const doesnt work
+        _.vertex(v1.x, v1.y);
+        _.vertex(v2.x, v2.y);
+        _.vertex(v3.x, v3.y);
+        _.endShape();
+    }
+
+    p.draw = () => {
+        if (_.frameCount % 50 === 0) console.log("frameRate:\t" + _.frameRate());
+        updateKdTree();
+        _.background(0);
+
+        for (let i = 0; i < maxVertices; i++) {
+            const v1: Vertex = vertices[i];
+            v1.update();
+            v1.render();
+            const kIndices: number[] = kdTree.knn([v1.x, v1.y], kValue, distanceThreshold);
+            if (kIndices.length === kValue) { // ignore self
+                const v_1: Vertex = vertices[kIndices[0]];
+                const v2: Vertex = vertices[kIndices[1]];
+                const v3: Vertex = vertices[kIndices[2]];
+                render(v_1, v2, v3);
+            }
+        }
+    };
+
+    // use index for loop over map due to performance
+    function updateKdTree() {
+        for (let i = 0; i < maxVertices; i++) {
+            coordinates[i] = [vertices[i].x, vertices[i].y];
+        }
+        kdTree = createKDTree(coordinates);
+    }
+
+    // determine the P5.Color that should be assigned to the triangle (or mesh stroke) given 3 vertices
     function determineColour(v1: Vertex, v2: Vertex, v3: Vertex): P5.Color {
         const dist12 = _.dist(v1.x, v1.y, v2.x, v2.y);
         const dist23 = _.dist(v2.x, v2.y, v3.x, v3.y);
@@ -65,57 +110,5 @@ new P5((p: P5) => {
 
         colorPost.setAlpha(area / 100.0);
         return colorPost;
-    }
-
-    function render(v1: Vertex, v2: Vertex, v3: Vertex) {
-        if (fillShape) {
-            _.stroke(255, 10);
-            //_.fill(v3.cAlpha);
-            _.fill(determineColour(v1, v2, v3));
-        } else {
-            _.noFill();
-            _.strokeWeight(1);
-            _.stroke(200, 20);
-        }
-        // shape
-        _.beginShape(0x0004); // 4 ==="TRIANGLES" hardcoded because global const doesnt work
-        _.vertex(v1.x, v1.y);
-        _.vertex(v2.x, v2.y);
-        _.vertex(v3.x, v3.y);
-        _.endShape();
-    }
-
-    p.draw = () => {
-        if (_.frameCount % 50 === 0) console.log("frameRate:\t" + _.frameRate());
-        updateKdTree();
-        _.background(0);
-
-
-        for (let i = 0; i < maxVertices; i++) {
-            const v1: Vertex = vertices[i];
-            v1.update();
-            v1.render();
-
-            // console.log("v1: " + v1.toString());
-            const kIndices: number[] = kdTree.knn([v1.x, v1.y], kValue, distanceThreshold);
-            //const v2: Vertex = vertices[kIndices[0]];
-            //const v3: Vertex = vertices[kIndices[1]];
-            // console.log("ret k length: " + kIndices.length);
-            if (kIndices.length === kValue) { // ignore self
-                const v_1: Vertex = vertices[kIndices[0]]; // repeat self?
-                const v2: Vertex = vertices[kIndices[1]];
-                const v3: Vertex = vertices[kIndices[2]];
-                //console.log("tv2: " + v2.toString() + "\tv3: " + v3.toString());
-                render(v_1, v2, v3);
-            }
-        }
-    };
-
-    // use index for loop over map due to performance
-    function updateKdTree() {
-        for (let i = 0; i < maxVertices; i++) {
-            coordinates[i] = [vertices[i].x, vertices[i].y];
-        }
-        kdTree = createKDTree(coordinates);
     }
 });
