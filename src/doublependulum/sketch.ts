d85b6c739e4b79d891f034810c659baa81a40c7a
phaserush <leozhang888@yahoo.ca>
2020-03-17 23:03:41 -0700

    cleaned up double pendulum sketch variables

--- src/doublependulum/sketch.ts
+++ src/doublependulum/sketch.ts
@@ -2,14 +2,7 @@ import * as P5 from 'p5'
 import {DoublePendulum,} from "./DoublePendulum";
 import {frameEpsilon} from "../Util/Util";
 
-let _: P5, r1: number, r2: number, m1: number, m2: number, a1: number, a2: number, a_v1: number, a_v2: number,
-    cx: number, cy: number;
-
-let nodeColor;
-const histLength = 255;
-// trail history color
-let historyColour: P5.Color;
-
+let _: P5;
 let pendulum: DoublePendulum;
 
 
@@ -25,18 +18,19 @@ new P5((p: P5) => {
     };
 
     function init(): void {
-        r1 = _.height / 5;
-        r2 = _.height / 5;
-        m1 = 20;
-        m2 = 10;
-        a1 = _.PI / _.randomGaussian(2, 0.4) * _.random([1, -1]);
-        a2 = _.PI / _.randomGaussian(2, 0.4) * _.random([1, -1]);
-        a_v1 = 0;
-        a_v2 = 0;
-        cx = _.width / 2;
-        cy = _.height / 4;
-        nodeColor = _.color("0xFFFFFF");
-        historyColour = _.color(0xe0, 0x22, 0xba); // #e022ba
+        const r1 = _.height / 5;
+        const r2 = _.height / 5;
+        const m1 = 20;
+        const m2 = 10;
+        const a1 = _.PI / _.randomGaussian(2, 0.4) * _.random([1, -1]);
+        const a2 = _.PI / _.randomGaussian(2, 0.4) * _.random([1, -1]);
+        const a_v1 = 0;
+        const a_v2 = 0;
+        const cx = _.width / 2;
+        const cy = _.height / 4;
+        const nodeColor = _.color("0xFFFFFF");
+        const historyColour = _.color(0xe0, 0x22, 0xba); // #e022ba
+        const histLength = 255;
 
         pendulum = new DoublePendulum(
             _, 1, r1, r2, m1, m2, a1, a2, a_v1, a_v2, cx, cy, nodeColor, historyColour, histLength,
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: './src/purityring/sketch.ts', // Point this to the file you want to build!
+    entry: './src/doublependulum/sketch.ts', // Point this to the file you want to build!
 
     output: {
         filename: "engine.js",
